e2ab525aa0f6453c7574e63b2e62a425fceaef4a
ndaalawneh <ndaalawneh@gmail.com>
2022-07-26 09:50:08 +0300

    main

--- react_redux/src/App.js
+++ react_redux/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Admin from "./components/admin";
+import Editpost from "./components/admin/Editpost";
+import Addpost from "./components/admin/Addpost";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/admin/edit" element={<Editpost />} />
+          <Route path="/admin/add" element={<Addpost />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
